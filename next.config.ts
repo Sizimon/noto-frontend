f5c4d35afa4ef0e49c2357412f5c58f77dcafb76
Shhmon <Siizmonsus@gmail.com>
2025-07-23 22:53:59 +0200

    FIX: Attempt to fix incorrectly loading css

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  basePath: '/noto-dashboard',
+  assetPrefix: '/noto-dashboard',
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
