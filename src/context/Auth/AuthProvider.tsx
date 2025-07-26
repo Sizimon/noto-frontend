aee93e3d8aa5aed14c0ba2384245d4f75b0a625e
Shhmon <Siizmonsus@gmail.com>
2025-07-26 16:08:48 +0200

    FEAT: Display serverside error rathen than a generic set error.

--- src/context/Auth/AuthProvider.tsx
+++ src/context/Auth/AuthProvider.tsx
@@ -51,6 +51,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
             } catch (error) {
                 setIsAuthenticated(false);
                 setUser(null);
+                console.error('Error fetching user data:', error);
             } finally {
                 setIsLoading(false);
             }
